eda4e818677ebe1fed10de4052817f72baed1cf0
JHillard <jhillard3.14@gmail.com>
2016-02-10 21:56:43 -0800

    Working AngleChar

--- Client IV/AngleOfAcceptanceCharacterizer.py
+++ Client IV/AngleOfAcceptanceCharacterizer.py
@@ -12,19 +12,19 @@ global left; left = b"L"
 global right; right  = b"R"
 global up; up  = b"U"
 global down; down = b"D"
-global Query; Query = b"Q"
+global query; query = b"Q"
 global goto; goto = b"G"
 global stop; stop = b"X"
 global blink; blink =b"B"
 
-#try:
-#    try:
-#        SER = serial.Serial('/dev/ttyACM1', baudrate = 250000)  # open serial port
-#    except:
-#        SER = serial.Serial('/dev/ttyACM0', baudrate = 250000)  # open serial port
-#except: raise Exception("Couldn't auto-connect. Are we plugged in?");
+try:
+   try:
+       SER = serial.Serial('/dev/ttyACM0', baudrate = 250000)  # open serial port
+   except:
+       SER = serial.Serial('/dev/ttyACM1', baudrate = 250000)  # open serial port
+except: raise Exception("Couldn't auto-connect. Are we plugged in?");
 
-#print("Teensy connected at " + SER.name)         # check which port was really used
+print("Teensy connected at " + SER.name)         # check which port was really used
 
 def moveLeft():
     SER.write(left);
@@ -37,17 +37,17 @@ def moveUp():
 
 def moveDown():
     SER.write(down);
-def stop():
+def Stop():
     SER.write(stop)
 def setSpeed(speed):
     SER.write( bytes( str(speed), "UTF-8"));
 
-def query():
+def Query():
     SER.flushInput()
-    SER.write(Query)
-    recieved = SER.readline()
+    SER.write(query)
+    recieved = (SER.readline()).decode()
     t = str(recieved).split(" ");
-    return [int(t[0].split("'")[-1] ), int(t[1].split("'")[-1]), int( t[-1].split("\\")[0] ) ];
+    return [int(t[0].split("'")[-1] ), int(t[1].split("'")[-1]), int( t[2].split("\\")[0] ) ];
 
 
 
@@ -56,28 +56,39 @@ def scanTwoLines(azTime, altTime, sampleNum = 100):
     lineData = [];
     moveLeft()
     for i in range(1,sampleNum):
-        lineData.append(query());
+        lineData.append(Query());
+        print("waiting for: " + str(azTime/sampleNum) + " : " + str(i));
         time.sleep(azTime/sampleNum);
-    stop();
+    Stop();
     moveDown();
+    print(altTime)
     time.sleep(altTime);
-    stop();
+    Stop();
+    time.sleep(1)
     moveRight();
+    moveRight();
+    print("why am I not moving?")
     for i in range(1,sampleNum):
-        lineData.append(query());
+        lineData.append(Query());
+        print(str(lineData[-1]))
         time.sleep(azTime/sampleNum);
-    stop();
+    Stop();
     return lineData;
 
 def rasterScan(azTime, altTime, rows = 10, rowSampleNum  = 100):
-    rows = rows/2;
+
     rasterData = []
-    for i in range(1, rows-1):
-        rasterData.append(scanTwoLines(azTime, altTime/rows, sampleNum = rowSampleNum))
+    for i in range(1, int(rows)-1):
+        print("Hot Pocket Cooked")
+        [rasterData.append(lineData) for lineData in (scanTwoLines(azTime, altTime/rows, sampleNum = rowSampleNum))]
         moveDown();
-        time.sleep(altTime);
-        stop();
-    scanTwoLines(azTime, altTime/rows, sampleNum = rowSampleNum)
+        print("Line Done")
+        time.sleep(altTime/rows);
+        Stop();
+        time.sleep(1)
+    [rasterData.append(lineData) for lineData in (scanTwoLines(azTime, altTime/rows, sampleNum = rowSampleNum))]
+    from pprint import pprint;
+    pprint(rasterData)
     writeCSV(rasterData);
 
 def writeCSV(data):
@@ -86,8 +97,9 @@ def writeCSV(data):
 		for row in data:
 			writer.writerow(row)
 
-data = []
-for i in range(1,5):
-	data. append(["asdas", 56]);
-writeCSV(data)
-    
+lolWut = []
+for i in range(1,10):
+    lolWut.append([1,2,3])
+writeCSV(lolWut);
+# rasterScan(1, 1);
+   
